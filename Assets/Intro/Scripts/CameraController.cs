3e9d94795d3ef1bfe63611ff5469c7caafbb30bc
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-08-23 11:45:34 +1000

    23/08/2018

---
+++ Assets/Intro/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Intro
+{
+    public class CameraController : MonoBehaviour
+    {
+
+        public Transform target;
+        private Vector3 offset;
+
+        // Use this for initialization
+        void Start()
+        {
+            offset = transform.position - target.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = target.position + offset;
+
+        }
+    }
+}
---
+++ Assets/SteeringBehaviours/Scripts/AI/AIAgentDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviours
+{
+    public class AIAgentDirector : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
