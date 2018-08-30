d8e75e6ab37e502b50bd8c6c3c2b67c1617a1e66
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-08-30 11:57:24 +1000

    30/08/2018

---
+++ Assets/SteeringBehaviours/Scripts/Cameras/CameraBounds.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SteeringBehaviours
+{
+    public class CameraBounds : MonoBehaviour
+    {
+        public Vector3 size = new Vector3(50, 0, 20);
+
+        public Vector3 GetAdjustedPosition(Vector3 incomingPos)
+        {
+            Vector3 pos = transform.position;
+            Vector3 halfSize = size * 0.5f;
+            
+            if (incomingPos.x > pos.x + halfSize.x)
+            {
+                incomingPos.x = pos.x + halfSize.x;
+            }
+            if (incomingPos.x < pos.x - halfSize.x)
+            {
+                incomingPos.x = pos.x - halfSize.x;
+            }
+            if (incomingPos.y > pos.y + halfSize.y)
+            {
+                incomingPos.y = pos.y + halfSize.y;
+            }
+            if (incomingPos.y < pos.y - halfSize.y)
+            {
+                incomingPos.y = pos.y - halfSize.y;
+            }
+            if (incomingPos.z > pos.z + halfSize.z)
+            {
+                incomingPos.z = pos.z + halfSize.z;
+            }
+            if (incomingPos.z < pos.z - halfSize.z)
+            {
+                incomingPos.z = pos.z - halfSize.z;
+            }
+            return Vector3.zero;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            GetAdjustedPosition(transform.position);
+        }
+    }
+}
